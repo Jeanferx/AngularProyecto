c974aea4f6435c92babd74ad4379d64bf9a86b6d
jean castro <castrojean848@gmail.com>
2025-11-20 20:47:03 -0500

    Descripción de los cambios

--- src/app/lista/lista.ts
+++ src/app/lista/lista.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-lista',
   imports: [CommonModule, HttpClientModule],
@@ -9,13 +10,15 @@ import Swal from 'sweetalert2';
   styleUrls: ['./lista.css'],
 })
 export class Lista {
+
   Productos: any[] = []; // Lista que vendrá de la API
   private apiUrl = 'https://localhost:7010/WeatherForecast';
-
 
   constructor(private http: HttpClient) {
     this.cargarUsuarios();
   }
+
+  // Cargar IDs y luego traer cada producto por ID
   cargarUsuarios() {
     this.http.get<string[]>(`${this.apiUrl}/lista-ids`).subscribe({
       next: (ids) => {
@@ -29,8 +32,44 @@ export class Lista {
       }
     });
   }
+  Editar(productos: any) {
+    Swal.fire({
+      title: 'Editar Producto',
+      html: `
+      <label>Nombre</label>
+      <input id="swal-nombre" class="swal2-input" value="${productos.name}">
+
+      <label>Descripcion</label>
+      <input id="swal-descripcion" class="swal2-input" value="${productos.description}">
+
+      <label>Valor</label>
+      <input id="swal-valor" class="swal2-input" value="${productos.valor}">
+    `,
+      showCancelButton: true,
+      confirmButtonText: 'Guardar',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (!result.isConfirmed) return;
+
+      const nuevoNombre = (document.getElementById('swal-nombre') as HTMLInputElement).value;
+      const nuevaDescripcion = (document.getElementById('swal-descripcion') as HTMLInputElement).value;
+      const nuevoValor = (document.getElementById('swal-valor') as HTMLInputElement).value;
+
+      // Actualiza los valores del producto
+      productos.name = nuevoNombre;
+      productos.description = nuevaDescripcion;
+      productos.valor = nuevoValor;
+
+      // PUT a la API
+      this.http.put(`${this.apiUrl}/${productos.id}`, productos).subscribe(() => {
+        Swal.fire("Listo", "Producto actualizado correctamente", "success");
+      });
+    });
+  }
 
 
+
+  // Eliminar
   eliminarUsuario(productos: any) {
     Swal.fire({
       title: '¿Estás seguro?',
@@ -45,10 +84,10 @@ export class Lista {
       color: '#004e92'
     }).then((result) => {
       if (result.isConfirmed) {
-        // Llamada DELETE a la API
         this.http.delete(`${this.apiUrl}/${productos.id}`).subscribe({
           next: () => {
             this.Productos = this.Productos.filter(u => u.id !== productos.id);
+
             Swal.fire({
               title: 'Eliminado',
               text: `${productos.nombre} ha sido eliminado correctamente.`,
@@ -67,13 +106,14 @@ export class Lista {
     });
   }
 
+  // Leer
   leerUsuario(productos: any) {
     Swal.fire({
       title: `${productos.name}`,
       html: `
-        <p><strong>name:</strong> ${productos.name}</p>
-        <p><strong>Description:</strong> ${productos.Description}</p>
-        <p><strong>Valor:</strong> ${productos.Valor}</p>
+        <p><strong>Nombre:</strong> ${productos.name}</p>
+        <p><strong>Descripción:</strong> ${productos.description}</p>
+        <p><strong>Valor:</strong> ${productos.valor}</p>
       `,
       icon: 'info',
       confirmButtonText: 'Cerrar',
